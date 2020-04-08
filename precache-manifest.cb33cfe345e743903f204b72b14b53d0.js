381ecfb157770c311a97dce5f511b2a753d86aa1
CasmirChuks <casmirakachi@gmail.com>
2020-04-08 16:00:43 +0200

    Updates

---
+++ precache-manifest.cb33cfe345e743903f204b72b14b53d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598d5b3c7db08d7312ad9d71c4994f21",
+    "url": "/roboTech/index.html"
+  },
+  {
+    "revision": "23f463c46ca7833d6ade",
+    "url": "/roboTech/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "043345b206c8f6f8ea08",
+    "url": "/roboTech/static/css/main.b9aa852a.chunk.css"
+  },
+  {
+    "revision": "23f463c46ca7833d6ade",
+    "url": "/roboTech/static/js/2.0f28cf9b.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/roboTech/static/js/2.0f28cf9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "043345b206c8f6f8ea08",
+    "url": "/roboTech/static/js/main.e24af917.chunk.js"
+  },
+  {
+    "revision": "d1a3c2b822bdc4832fd9",
+    "url": "/roboTech/static/js/runtime-main.243d792c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/roboTech/static/media/SEGA.12a3db8b.woff"
+  }
+]);
