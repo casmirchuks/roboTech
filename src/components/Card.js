c7719c12d29e643fb147f8d3cde2a4b2a18fdbe8
CasmirChuks <casmirakachi@gmail.com>
2020-04-08 14:09:05 +0200

    roboLaunch

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = ({name, email, id}) => {
+    return (
+        <div className='tc bg-light-green dib br3 pa1 ma2 grow b2 shadow-6'>
+            <img alt='robo' src={`https://robohash.org/${id}?200x200`} />
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
